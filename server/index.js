f6311bb8f1b19c12e05e2717e059e3d2129edfd6
Shashi-1904 <shashikantpawase71@gmail.com>
2024-04-26 17:21:22 +0530

    Added

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const app = require('./config/express');
 const config = require('./config/config')
+const express=require('express')
+const path=require('path')
 
 // initialize mongo
 require('./config/mongoose');
+
+app.use(express.json());
+
+const _dirname=path.dirname("")
+
+const buildpath = path.join(_dirname,"../build")
+
+app.use(express.static(buildpath));
 
 // listen to the port 
 app.listen(config.port, () => {
